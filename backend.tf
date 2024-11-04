7b14158c3089e265c41d98f9accbae69700c52ef
Farhan <farhanfaiz05@gmail.com>
2024-11-04 20:24:09 +0530

    test s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = ""
+    bucket         = "farhan-g2-test-bucket"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
